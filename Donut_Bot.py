e68f0afcde196d9e848cf9fc8b5c79c4d6257545
Ja5onChen <131934574+Ja5onChen@users.noreply.github.com>
2023-06-23 10:32:49 +1200

    created 4 versions of clickandcollect
    created 4 versions of clickandcollect

--- Donut_Bot.py
+++ Donut_Bot.py
@@ -1,22 +1,18 @@
 import random
 from random import randint
-
 #list of random names
 names = ["Mark", "Pheobe", "Michael", "Denise", "Ellen", "Eric", "Lewis", "Lana", "Moana", "Sally" ]
-
 def welcome():
     '''
     purpose: To generate a random name from the list and print out a welcome message
     Parameters: none
     Returns: none
     '''
-
 num = randint(0,9)
 name = (names[num])
 print("***Welcome to Dream Donut***")
 print("***My name is",name,"****")
 print("*** I will be here to help you order your delicious dream donut ***")
-
 def main():
     '''
     Purpose: To run all functions
@@ -25,5 +21,4 @@ def main():
     Returns: None
     '''
     welcome()
-    
 main()
