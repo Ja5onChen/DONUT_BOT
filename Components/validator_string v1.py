43014b93346f362828af2bd2f299f0f7233419fd
Ja5onChen <131934574+Ja5onChen@users.noreply.github.com>
2023-07-26 14:59:11 +1200

    Finished validator string v1

---
+++ Components/validator_string v1.py
@@ -0,0 +1,10 @@
+question = "Enter your name "
+
+while True:
+    response = input(question)
+    x = response.isalpha()
+    if x == False:
+        print("Input must only contain letters")
+    else:
+        print(response.title())
+        break
